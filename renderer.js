7b80fc59ffa37dfca86b87c9df674bfbc8867848
rgb_beard <62804472+rgbbeard@users.noreply.github.com>
2021-09-14 13:27:47 +0200

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -1,67 +1,92 @@
-function elementsRenderer() {
-    //Validate addresses
-    _('[validateaddress]').forEach(input => {
-        input.onkeyup = function() {
-            //Remove commas
-            if(this.value.match(/\,+/i)) {
-                let v = this.value.replaceAll(",", "");
-                this.value = v;
-            }
-        };
-    });
-    //Validate email
-    _('[validatemail]').forEach(input => {
-        function VerifyValue(ipt) {
-            let form = findElement({tag: "form"}, ipt, (form) => {
-                let btn = form.querySelector('[formsubmitter]');
-                if(ipt.value.match(/([a\.\--z\_]*[a0-z9]+@)([a-z]+\.)([a-z]{2,6})/i)) {
-                    if(btn.isDisabled()) {
-                        btn.removeAttribute("disabled");
-                    }
-                } else {
-                    btn.setAttribute("disabled", "true");
-                }
-            });
-        }
-        VerifyValue(input);
-        input.onkeyup = VerifyValue(input);
-    });
-    //Validate italian fiscal code
-    _('[validateitfcode]').forEach(input => {
-        function VerifyValue(ipt) {
-            let form = findElement({tag: "form"}, ipt, (form) => {
-                let btn = form.querySelector('[formsubmitter]');
-                if(ipt.value.match(/(\D{6})(\d\d)(\D\d\d)(\D\d\d)(\d\D)/i)) {
-                    if(btn.isDisabled()) {
-                        btn.removeAttribute("disabled");
-                    }
-                } else {
-                    btn.setAttribute("disabled", "true");
-                }
-            });
-        }
-        VerifyValue(input);
-        input.onkeyup = VerifyValue(input);
-    });
-    //Show/hide password
-    _(".input-group.password-input").forEach(pi => {
-        let
-            password_shown = false,
-            password_field = pi.querySelector("input");
+Objects.prototype.rippleAnimation = function(e) {
+        e = window.event;
+        let t = e.target;
 
-        if(!isNull(pi.querySelector("span"))) {
-            pi.querySelector("span").onclick = function() {
-                if(password_shown) {
-                    this.removeAttribute("password-shown");
-                    password_field.setAttribute("type", "password");
-                    password_shown = false;
-                } else {
-                    this.setAttribute("password-shown", "true");
-                    password_field.setAttribute("type", "text");
-                    password_shown = true;
-                }
-            };
+        t.removeClass("animated");
+
+        if(!t.hasClass("animated")) {
+                t.addClass("animated");
         }
-    });
+
+        setTimeout(function() {
+                t.removeClass("animated");
+        }, 700);
+};
+
+function elementsRenderer() {
+        //Validate addresses
+        _('[validateaddress]').forEach(input => {
+                input.onkeyup = function() {
+                        //Remove commas
+                        if(this.value.match(/\,+/i)) {
+                                let v = this.value.replaceAll(",", "");
+                                this.value = v;
+                        }
+                };
+        });
+    
+        //Validate email
+        _('[validatemail]').forEach(input => {
+                function VerifyValue(ipt) {
+                        let form = findElement({tag: "form"}, ipt, (form) => {
+                                let btn = form.querySelector('[formsubmitter]');
+                                if(ipt.value.match(/([a\.\--z\_]*[a0-z9]+@)([a-z]+\.)([a-z]{2,6})/i)) {
+                                        if(btn.isDisabled()) {
+                                                btn.removeAttribute("disabled");
+                                        }
+                                } else {
+                                        btn.setAttribute("disabled", "true");
+                                }
+                        });
+                }
+                VerifyValue(input);
+                input.onkeyup = VerifyValue(input);
+        });
+
+        //Validate italian fiscal code
+         _('[validateitfcode]').forEach(input => {
+                function VerifyValue(ipt) {
+                        let form = findElement({tag: "form"}, ipt, (form) => {
+                                let btn = form.querySelector('[formsubmitter]');
+                                if(ipt.value.match(/(\D{6})(\d\d)(\D\d\d)(\D\d\d)(\d\D)/i)) {
+                                        if(btn.isDisabled()) {
+                                                btn.removeAttribute("disabled");
+                                        }
+                                } else {
+                                        btn.setAttribute("disabled", "true");
+                                }
+                        });
+                }
+                VerifyValue(input);
+                input.onkeyup = VerifyValue(input);
+        });
+
+        //Show/hide password
+        _(".input-group.password-input").forEach(pi => {
+                let
+                password_shown = false,
+                password_field = pi.querySelector("input");
+
+                if(!isNull(pi.querySelector("span"))) {
+                        pi.querySelector("span").onclick = function() {
+                                if(password_shown) {
+                                        this.removeAttribute("password-shown");
+                                        password_field.setAttribute("type", "password");
+                                        password_shown = false;
+                                } else {
+                                        this.setAttribute("password-shown", "true");
+                                        password_field.setAttribute("type", "text");
+                                        password_shown = true;
+                                }
+                        };
+                }
+        });
+
+        //Ripple animate button
+        _(".btn-ripple").forEach(b => {
+                b.onclick = () => {
+                        this.rippleAnimation();
+                };
+        });
 }
 SystemFn(elementsRenderer);
