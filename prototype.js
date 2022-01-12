3ab8b82526ec2c187d51c07070d61ded967847b0
rgb_beard <62804472+rgbbeard@users.noreply.github.com>
2022-01-12 08:46:41 +0100

    Update prototype.js

--- prototype.js
+++ prototype.js
@@ -6,18 +6,38 @@ Array.prototype.inArray = function (obj) {
 	return false;
 };
 Array.prototype.globalAction = function (listener, fn) { this.forEach(a => _(a).addEventListener(listener, fn)); };
-//Array.prototype.last = this.reverse()[0];
-Object.prototype.hasClass = function (c) { return this.classList.contains(c); };
-Object.prototype.toggleClass = function (c) { this.classList.toggle(c); };
-Object.prototype.toggleClasses = function (...args) { args.forEach(c => this.classList.toggle(c)); };
-Object.prototype.removeClass = function (c) { this.classList.remove(c); };
-Object.prototype.removeClasses = function (...args) { args.forEach(c => this.removeClass(c)); };
-Object.prototype.addClass = function (c) { this.classList.add(c); };
-Object.prototype.addClasses = function (...args) { args.forEach(c => this.addClass(c)); };
-Object.prototype.removeAttributes = function (...args) { args.forEach(a => this.removeAttribute(a)); };
+Object.prototype.hasClass = function (c) { return this.classList.contains(String(c)); };
+Object.prototype.toggleClass = function (c) { this.classList.toggle(String(c)); };
+Object.prototype.toggleClasses = function (...args) { args.forEach(c => this.classList.toggle(String(c))); };
+Object.prototype.removeClass = function (c) { this.classList.remove(String(c)); };
+Object.prototype.removeClasses = function (...args) { args.forEach(c => this.removeClass(String(c))); };
+Object.prototype.addClass = function (c) { this.classList.add(String(c)); };
+Object.prototype.addClasses = function (...args) { args.forEach(c => this.addClass(String(c))); };
+Object.prototype.removeAttributes = function (...args) { args.forEach(a => this.removeAttribute(String(a))); };
 Object.prototype.appendChildren = function (...args) { args.forEach(child => this.appendChild(child)); };
 Object.prototype.removeChildren = function (...args) { args.forEach(child => this.removeChild(child)); };
 Object.prototype.insertAfter = function (newElement) { this.parentNode.insertBefore(newElement, this.nextElementSibling); };
+Object.prototype.isFirstChild = function() {
+	let
+		parent = this.parentNode,
+		fs = parent.children[0];
+		
+	return this == fs ? true : false;
+};
+Object.prototype.isLastChild = function() {
+	let
+		parent = this.parentNode,
+		c = parent.children,
+		ls = parent.children[c.length - 1];
+
+	return this == ls ? true : false;	
+};
+Object.prototype.previousSibling = function() {
+	return this.isFirstChild() ? null : this.previousElementSibling;
+};
+Object.prototype.nextSibling = function() {
+	return this.isLastChild() ? null : this.nextElementSibling;
+}
 Object.prototype.moveBefore = function() {
     let parent = this.parentNode, prev = this.previousElementSibling;
 	if(!isNull(prev)) {
@@ -56,7 +76,9 @@ Object.prototype.addId = function (id) {
 				break;
 			}
 		}
-		if (hasId === false) this.setAttribute("id", ids.join(" "));
+		if (hasId === false) {
+			this.setAttribute("id", ids.join(" "));
+		}
 	}
 };
 Object.prototype.hasId = function(id) {
@@ -64,28 +86,35 @@ Object.prototype.hasId = function(id) {
 	 if(!isNull(ids)) {
 		ids = ids.split(" ");
 		ids.forEach(i => {
-			if(i == id.toString()) return true;
+			if(i == id.toString()) {
+				return true;
+			}
 		});
 	 }
 	 return false;
 };
 Object.prototype.elementIndex = function (element) {
-	for (let i = 0; i < this.length; i++) {
-		if (this[i] === element) return i;
+	for (let i = 0; i < this.children.length; i++) {
+		if (this.children[i] === element) {
+			return i;
+		}
 	}
 	return -1;
 };
 Object.prototype.addStyles = function (styles = {}) {
 	let temp = [];
 	for(let style in styles) {
-		if(styles[style].isFunction()) continue;
+		if(styles[style].isFunction()) {
+			continue;
+		}
 		temp.push(String(style+":"+styles[style]));
 	}
 	this.setAttribute("style", temp.join(";"));
 };
-Object.prototype.length = Object.keys(this).length;
+Object.prototype.length = function() { return Object.keys(this).length; };
 Object.prototype.isDisabled = function() { return isNull(this.getAttribute("disabled")) ? false : true; };
-Object.prototype.onenter = function(e, fn) {
+Object.prototype.onenter = function(fn, e) {
+	e = window.event;
 	if(e.key == "Enter" || e.which == 13 || e.code == "Enter") {
 		fn.call();
 	}
@@ -99,16 +128,16 @@ String.prototype.int = function() {
 	return parseInt(this);
 };
 Number.prototype.percentage = function (percentage) { return percentage / 100 * this; };
-Number.prototype.inRange = function (min, max) { return this >= parseInt(min) && this <= parseInt(max) ? true : false; };
+Number.prototype.inRange = function (min, max) { return this >= parseInt(min) && this <= parseInt(max); };
 Javascript.prototype.isArray = function() { return Array.isArray(this); };
 Javascript.prototype.isFunction = function () { return this && {}.toString.call(this) === '[object Function]'; };
 Javascript.prototype.bool = function () {
 	let str = String(this).replace(/[\s|\t|\n]+/gm, "");
-	return str.match(/true/i) || str.match(/yes/i) || str.match(/y/i) || str === "1" ? true : false;
+	return str.match(/^true$/i) || str.match(/^yes$/i) || str.match(/^y$/i) || str === "1" ? true : false;
 };
 Javascript.prototype.empty = function () {
 	let
-		target = String(this),
-		is_empty = (target.match(/[\s|\t|\n]+/gm) || target === "" || target === "null" || target === "undefined" || (this.isArray() === true && this.length < 1)) ? true : false;
+		target = String(this).toLowerCase(),
+		is_empty = target.match(/[\s|\t|\n]+/gm) || target === "" || target === "null" || target === "undefined" || this.length == 0 ? true : false;
 	return is_empty;
 };
